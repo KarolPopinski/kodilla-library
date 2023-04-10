ea10f977b3c9411c5d561b0e8c0ead44307f8625
Karol Popinski <k.popinski@gmail.com>
2023-04-10 14:26:25 +0200

    first commit

---
+++ src/test/java/com/kodillalibrary/library/KodillaLibraryApplicationTests.java
@@ -0,0 +1,8 @@
+package com.kodillalibrary.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaLibraryApplicationTests {
+}
